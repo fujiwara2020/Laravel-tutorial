26e2035f85987635ad76961b299005d510386ee7
yuya fujiwara <yuya.0509f@gmail.com>
2023-04-13 23:07:52 +0900

    タスク編集機能追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -24,3 +24,5 @@ Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm
 Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
 Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
 Route::post('/folders/create', [FolderController::class, 'create']);
+Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
